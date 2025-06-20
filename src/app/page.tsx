4b9a1038616d8a65eaa6425c6ba039d27e74a10e
zfeller <zfeller@ucsc.edu>
2025-06-20 13:47:50 -0700

    add in image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 export default function Home() {
   const images = [
-    "/images/IMG_3025.jpeg",
+    "/images/IMG_5785.jpg",
     "/images/IMG_3027.jpeg",
     "/images/IMG_3028.jpeg",
     "/images/IMG_3029.jpeg",
@@ -16,7 +16,20 @@ export default function Home() {
 
   return (
     <div style={{ width: "100vw", height:"100vh", margin: 0 }}>
-      <Image src={images[0]} alt="Logo" width={50} height={50} />
+      <Box sx={{ width: "100%", height: "500px", position: "relative", overflow: "hidden" }}>
+            <Image
+              src={images[0]}
+              alt={images[0] ?? "placeholder"}
+              fill
+              style={{ objectFit: "cover", marginTop:"70px"}}
+            />
+            <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.4)', zIndex: 1 }} />
+            <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: 2 }}>
+              <Typography variant="h3" sx={{ color: 'white', fontWeight: 'bold', fontFamily: 'Creato Bold', marginTop: '15px', textShadow: '2px 2px 6px rgba(0,0,0,0.8)' }}>
+                Woodstock&apos;s Employee Alliance
+              </Typography>
+            </Box>
+      </Box>
       
         <Box sx={{display:'flex', flexDirection:'column', marginTop:"100px"}}>
           <Box sx={{display:'flex', justifyContent:'center'}}>
