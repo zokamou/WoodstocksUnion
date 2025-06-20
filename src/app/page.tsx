8b26d58f9e51bb47def04855acd3c44102988f0a
zfeller <zfeller@ucsc.edu>
2025-06-20 13:47:45 -0700

    images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
 // import ImageCarousel from "./Components/Carousel";
 import {Box, Typography} from '@mui/material';
+import Image from 'next/image';
 
 // images 
 
 export default function Home() {
-  // const images = [
-  //   "/images/IMG_3025.jpeg",
-  //   "/images/IMG_3027.jpeg",
-  //   "/images/IMG_3028.jpeg",
-  //   "/images/IMG_3029.jpeg",
-  //   "/images/IMG_3030.jpeg",
-  //   "/images/IMG_3031.jpeg"
-  // ];
+  const images = [
+    "/images/IMG_3025.jpeg",
+    "/images/IMG_3027.jpeg",
+    "/images/IMG_3028.jpeg",
+    "/images/IMG_3029.jpeg",
+    "/images/IMG_3030.jpeg",
+    "/images/IMG_3031.jpeg"
+  ];
 
   return (
     <div style={{ width: "100vw", height:"100vh", margin: 0 }}>
-      {/* <ImageCarousel title={["Woodstock's Employee Alliance", "Join the Union!", "Ronan Sucks"]} images={images}/> */}
+      <Image src={images[0]} alt="Logo" width={50} height={50} />
+      
         <Box sx={{display:'flex', flexDirection:'column', marginTop:"100px"}}>
           <Box sx={{display:'flex', justifyContent:'center'}}>
             <Typography sx={{fontFamily:'Creato', fontWeight:'bold', fontSize: "32px"}}>
