6e33b6b664d71504974ede438b6dea9f3e6a10f7
zfeller <zfeller@ucsc.edu>
2025-06-20 13:27:54 -0700

    trying routes

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div style={{ width: "100vw", height:"100vh", margin: 0 }}>
+      <div style={{marginTop:"80px"}}>
+        hi
+      </div>
+    </div>
+  );
+}
---
+++ src/app/faqs/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ width: "100vw", height:"100vh", margin: 0 }}>
+      <div style={{marginTop:"80px"}}>
+        bye
+      </div>
+    </div>
+  );
+}
